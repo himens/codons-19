45aa0e7db054bc7964cffa27dd2832fbb292432a
Stefano Gallorini <himens@gmail.com>
2020-03-25 20:03:18 +0100

    Minor changes.

--- plot_data.py
+++ plot_data.py
@@ -10,26 +10,24 @@ gROOT.SetBatch(True);
 
 # Plot provnince
 province = ["Bergamo", "Brescia", "Pisa", "Torino", "Padova", "Verona"]
-canv_prov = TCanvas("canv_prov", "", 1000, 1000) 
-
+pdf_name = "corona_prov.pdf"
 for prov in province:
-    canv_prov = corona_fit("full_data_ita_prov.csv", "PC province", 
-                           "ITA", prov,
-                           "total_cases",
-                           "test", 5, -1, 15)
-    if prov == province[0]:  canv_prov.SaveAs("corona_prov.pdf[")
-    canv_prov.SaveAs("corona_prov.pdf")
-    if prov == province[-1]: canv_prov.SaveAs("corona_prov.pdf]")
+    canv = corona_fit("full_data_ita_prov.csv", "PC province", 
+                      "ITA", prov,
+                      "total_cases",
+                      "test", 5, -1, 15)
+    if prov == province[0]:  canv.SaveAs(pdf_name + "[")
+    canv.SaveAs(pdf_name)
+    if prov == province[-1]: canv.SaveAs(pdf_name + "]")
 
 # Plot regioni
 regioni = ["", "Lombardia", "Toscana", "Piemonte", "Veneto"]
-canv_reg = TCanvas("canv_reg", "", 1000, 1000) 
-
+pdf_name = "corona_reg.pdf"
 for reg in regioni:
-    canv_reg = corona_fit("full_data_ita_reg.csv", "PC regioni", 
-                          "ITA", reg,
-                          "total_cases",
-                          "test", 5, -1, 15)
-    if reg == regioni[0]:  canv_reg.SaveAs("corona_reg.pdf[")
-    canv_reg.SaveAs("corona_reg.pdf")
-    if reg == regioni[-1]: canv_reg.SaveAs("corona_reg.pdf]")
+    canv = corona_fit("full_data_ita_reg.csv", "PC regioni", 
+                      "ITA", reg,
+                      "total_cases",
+                      "test", 5, -1, 15)
+    if reg == regioni[0]:  canv.SaveAs(pdf_name + "[")
+    canv.SaveAs(pdf_name)
+    if reg == regioni[-1]: canv.SaveAs(pdf_name + "]")
